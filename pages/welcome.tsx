e94fdb99c9d48b16d693d04fa208c0dd6ab37bd4
anso <anderson.floriano@solidaridadnetwork.org>
2022-12-09 18:06:13 -0300

    Add show error in welcome page

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,12 +1,35 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 import StyledButton from "../components/StyledButton";
 import StyledInput from "../components/StyledInput";
 
 export default function Welcome() {
+  const [name, setName] = useState('');
+  const [showError, setShowError] = useState(false);
+
+  function handlerInputChange(ev: any) {
+    setName(ev.target.value);
+    setShowError(false);
+  }
+
+  function handlerButtonClick() {
+    if (!name) {
+      setShowError(true);
+      return;
+    }
+
+    localStorage.setItem('name', name);
+    router.push('/keynotes');
+  }
+
   const router = useRouter();
   return <div className="bg-[#ffcc00] w-full flex h-screen flex-col items-center gap-4 pt-44">
     <p className="text-xl antialiased	font-semibold">Olá! Como você se chama?</p>
-    <StyledInput />
-    <StyledButton onClick={() => router.push('/keynotes')}>Enviar</StyledButton>
+    <StyledInput onChange={handlerInputChange} />
+    {
+      showError &&
+      <p className="text-red-500">Digite seu nome aí vai...</p>
+    }
+    <StyledButton onClick={handlerButtonClick}>Enviar</StyledButton>
   </div>
 }
